8fd4e2eb7f0a249d656fa455d345757b88f05963
KILKA <157209991+KILKA1001@users.noreply.github.com>
2025-05-24 13:59:28 +0300

    Update __init__.py

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,7 +1,21 @@
 from bot.commands.base import bot, run_monthly_top
-from .fines import fine
-from .fines import myfines
+from .fines import (
+fine,
+myfines,
+all_fines,
+finedetails,
+editfine,
+cancel_fine,
+finehistory,
+topfines
+)
 
 __all__ = ["bot", "run_monthly_top"]
 bot.add_command(fine)
 bot.add_command(myfines)
+bot.add_command(all_fines)
+bot.add_command(finedetails)
+bot.add_command(editfine)
+bot.add_command(cancel_fine)
+bot.add_command(finehistory)
+bot.add_command(topfines)
