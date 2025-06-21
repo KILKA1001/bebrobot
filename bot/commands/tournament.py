30b402f07c3d51f8c539f635d16f514f87f313d5
KILKA <157209991+KILKA1001@users.noreply.github.com>
2025-06-22 00:10:32 +0300

    Ох

--- bot/commands/tournament.py
+++ bot/commands/tournament.py
@@ -19,8 +19,7 @@ from bot.systems.tournament_logic import (
     handle_unregister
 )
 from bot.data.tournament_db import add_discord_participant as db_add_participant
-
-
+from bot.systems.tournament_logic import delete_tournament as delete_tournament_view
 # Import the bot instance from base.py instead of creating a new one
 from bot.commands.base import bot
 
@@ -68,6 +67,7 @@ async def tournamenthistory(ctx, limit: int = 10):
 @commands.has_permissions(administrator=True)
 async def deletetournament(ctx, tournament_id: int):
     """Удалить турнир и все связанные с ним записи."""
+    await delete_tournament_view(ctx, tournament_id)
 
 @bot.command(name="regplayer")
 @commands.has_permissions(administrator=True)
